8ffa76bc24f2e448459dab047cd9ab13ce7aeaf6
gopal <nago@skidata.com>
2019-09-01 19:22:31 +0530

    first commit

---
+++ src/main/java/com/ascent/hr/ascenthrmessageservice/AscentHrMessageServiceApplication.java
@@ -0,0 +1,13 @@
+package com.ascent.hr.ascenthrmessageservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AscentHrMessageServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AscentHrMessageServiceApplication.class, args);
+	}
+
+}
